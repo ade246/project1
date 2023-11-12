8e42c74f6ddcdadea9fc77299ea375c28d32b00d
Khari Wallace <khari.wallace@dcmail.ca>
2023-11-12 17:37:18 -0500

    coin/selection list and item costs/coin values

--- vending_machine.py
+++ vending_machine.py
@@ -7,3 +7,27 @@ November 15, 2023
 Khari Wallace <khari.wallace@dcmail.ca>
 
 """
+
+import PySimpleGUI as sg
+# Define custom font and size
+sg.theme('DarkAmber')
+font = ('Helvetica', 15)
+
+COIN_LIST = [
+    ("5\u00A2", "Nickel"),  # 5 cents
+    ("10\u00A2", "Dime"),   # 10 cents
+    ("25\u00A2", "Quarter"),# 25 cents
+    ("$1", "Loonie"),       # 1 dollar
+    ("$2", "Toonie")        # 2 dollars
+]
+
+SELECTION_LIST = [
+    ("surprise", "A"),  # Example of using single letter keys
+    ("pop", "B"),
+    ("chips", "C"),
+    ("choc", "D"),
+    ("beer", "E")
+]
+
+ITEM_COSTS = {"surprise": 500, "pop": 100, "chips": 150, "choc": 200, "beer": 300}
+COIN_VALUES = {"Nickel": 5, "Dime": 10, "Quarter": 25, "Loonie": 100, "Toonie": 200}
