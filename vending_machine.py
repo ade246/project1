9823d3aeeceaae67ec588907123c219088a6bf27
Khari Wallace <khari.wallace@dcmail.ca>
2023-11-12 17:33:18 -0500

    Commiting main program vending_machine.py

---
+++ vending_machine.py
@@ -0,0 +1,9 @@
+"""vending_machine_.py - simulate a coin operated vending machine
+### KHARI WALLACE, 100807131, TPRG 2131 ###
+
+
+TPRG 2131 Fall 2022 Project 1
+November 15, 2023
+Khari Wallace <khari.wallace@dcmail.ca>
+
+"""
