5b26ad574cca9ad14f3eb401a5931311d5983bd3
Khari Wallace <khari.wallace@dcmail.ca>
2023-11-13 09:52:24 -0500

    test_vending_machine.py is ready for marking

--- test_vending_machine.py
+++ test_vending_machine.py
@@ -1,7 +1,6 @@
 """test_vending_machine_.py - simulate a coin operated vending machine
 ### KHARI WALLACE, 100807131, TPRG 2131 ###
 
-
 TPRG 2131 Fall 2023 Project 1
 November 15, 2023
 Khari Wallace <khari.wallace@dcmail.ca>
