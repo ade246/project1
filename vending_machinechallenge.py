56e57873ae31a5d704fce891c8f9d50364b9597a
Khari Wallace <khari.wallace@dcmail.ca>
2023-11-13 17:43:44 -0500

    modifying vending_machine.py to create challenges

---
+++ vending_machinechallenge.py
@@ -0,0 +1,16 @@
+"""vending_machine_.py - simulate a coin operated vending machine
+### KHARI WALLACE, 100807131, TPRG 2131 ###
+Challenge 1,3,6
+
+
+Machine has a bulk set of 5 for each item. The quantity reaches zero after 5 purchases. After the 5th purchase is made a
+the item colour is faded out and when you try to select the item you will receive a message "(item) is sold out".(challenge 1,3)
+
+The price of the item is shown on the panel beside each selection.(challenge 6)
+
+
+TPRG 2131 Fall 2023 Project 1
+November 15, 2023
+Khari Wallace <khari.wallace@dcmail.ca>
+
+"""
