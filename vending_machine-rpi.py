3bd2f4efc966c16075a69a86065d45dc874e7852
Khari Wallace <khari.wallace@dcmail.ca>
2023-11-15 15:12:38 -0500

    Modified vending_machine.py to work with RPI hardware

---
+++ vending_machine-rpi.py
@@ -0,0 +1,10 @@
+"""vending_machine_.py - simulate a coin operated vending machine
+### KHARI WALLACE, 100807131, TPRG 2131 ###
+
+RPI Code
+
+TPRG 2131 Fall 2023 Project 1
+November 15, 2023
+Khari Wallace <khari.wallace@dcmail.ca>
+
+"""
